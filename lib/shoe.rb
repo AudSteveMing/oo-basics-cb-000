de052e6e67b7d8b0f99edc8ab9a96c97d20ca601
Steve Cordova <cordovas@bc.edu>
2020-07-25 23:34:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,5 @@
 class shoe
-  attr_accessor :brand, :color, :size, :material, :condition,
+  attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
